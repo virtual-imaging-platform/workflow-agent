b14c3c8de7c4e3cd47f6c2b9b61b3db65496a42a
Axel Bonnet <axel.bonnet@creatis.insa-lyon.fr>
2022-11-25 16:01:36 +0100

    implement delete

--- src/main/java/fr/insalyon/creatis/agent/workflow/command/KillCommand.java
+++ src/main/java/fr/insalyon/creatis/agent/workflow/command/KillCommand.java
@@ -4,6 +4,8 @@ import fr.insalyon.creatis.agent.workflow.database.JobDatabase;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 
 public class KillCommand implements Command {
@@ -15,24 +17,33 @@ public class KillCommand implements Command {
     }
 
     public void run() {
-        List<String> jobIds = this.jobDB.getJobIds();
+        Map<String,String> jobIds = this.jobDB.getJobsToKill();
 
-        for (String id : jobIds) {
-            try {
-                logger.info("Killing job id '" + id + "'");
-                String exec = "dirac-wms-job-kill " + id;
-                Process process = Runtime.getRuntime().exec(exec);
-                process.waitFor();
-                if (process.exitValue() != 0) {
-                    logger.error("Unable to kill job id '" + id + "'");
-                } else {
-                    this.jobDB.updateStatus(id);
-                }
-            } catch (InterruptedException | IOException e) {
-                logger.error("Error killing job " + id, e);
-            }
+        for (String id : jobIds.keySet()) {
+            // kill running jobs, delete waiting and succesfully submitted
+            boolean mustDelete = ! jobIds.get(id).equalsIgnoreCase("RUNNING");
+            killJob(id, mustDelete);
         }
 
         this.jobDB.close();
+    }
+
+    public void killJob(String id, boolean mustDelete) {
+        try {
+            String command = mustDelete ? "delete" : "kill";
+            logger.info("Doing " + command + "  on job id '" + id + "'");
+            String exec = "dirac-wms-job-" + command + " " + id;
+            Process process = Runtime.getRuntime().exec(exec);
+            process.waitFor();
+            if (process.exitValue() != 0) {
+                logger.error("Unable to " + command + " job id '" + id + "' with dirac");
+            } else if (mustDelete) {
+                this.jobDB.updateStatusToDeleted(id);
+            } else {
+                this.jobDB.updateStatusToCancelled(id);
+            }
+        } catch (InterruptedException | IOException e) {
+            logger.error("Error killing job " + id, e);
+        }
     }
 }
